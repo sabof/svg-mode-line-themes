80a0e01839cafbd66899202e7764c33231974259
Evgkeni Sampelnikof <esabof@gmail.com>
2015-04-25 21:06:18 +0100

    Version bump to 0.1.3

--- svg-mode-line-themes.el
+++ svg-mode-line-themes.el
@@ -1,5 +1,5 @@
 ;;; svg-mode-line-themes.el --- SVG-based themes for mode-line
-;;; Version: 0.1.2
+;;; Version: 0.1.3
 ;;; Package-Requires: ((xmlgen "0.4"))
 ;;; Author: sabof
 ;;; URL: https://github.com/sabof/svg-mode-line-themes
